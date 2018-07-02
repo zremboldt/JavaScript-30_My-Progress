084d387fff24c654cbc4564332d530381b948f07
Zac <zacaree@gmail.com>
2018-07-02 16:24:57 -0500

    Customized styling

---
+++ 010_Hold shift to check multiple boxes/script.js
@@ -0,0 +1,25 @@
+const checkboxes = document.querySelectorAll('.checklist input[type="checkbox"]');
+
+let mostRecentlyChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === mostRecentlyChecked) {
+        inBetween = !inBetween;
+        console.log('first / last');
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  // value of mostRecentlyChecked doesn't change until the interpreter gets to this line.
+  mostRecentlyChecked = this;
+}
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', handleCheck);
+})
