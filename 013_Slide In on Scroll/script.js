b3fe5164a18c10e72bc0be06a66bbc822647ac93
zacaree <zacharem@gmail.com>
2018-07-25 06:44:44 -0500

    013 is a work in progress

---
+++ 013_Slide In on Scroll/script.js
@@ -0,0 +1,29 @@
+
+
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  // console.count(e);
+  console.log(window.scrollY + window.innerHeight)
+  sliderImages.forEach(img => {
+    const slideInAt = (window.scrollY + window.innerHeight);
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
